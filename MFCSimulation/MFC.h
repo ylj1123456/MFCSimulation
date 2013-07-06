13ccdae17a0817168f564f1057d0207f3f056924
Lingjie Yu <ylj1123456@163.com>
2013-07-06 22:57:06 +0800

    add base class of mfc

--- MFCSimulation/MFC.h
+++ MFCSimulation/MFC.h
@@ -1,9 +1,51 @@
 #include<iostream>
 
+/*********************************
+* declaration
+**********************************/
 class CObject;
-
+class CCmdTarget;
+class CWinApp;
+class CWnd;
+class CView;
+class CFrameWnd;
+class CDocument;
+/********************************
+* definition
+*********************************/
 class CObject{
 public:
 	CObject(){}
 	~CObject(){}
+};
+class CCmdTarget:public CObject{
+public:
+	CCmdTarget() {}
+	~CCmdTarget(){}
+};
+class CWinApp:public CCmdTarget{
+public:
+	CWinApp(){}
+	~CWinApp(){}
+};
+class CWnd:public CCmdTarget{
+public:
+	CWnd(){}
+	~CWnd(){}
+};
+class CView:public CWnd{
+public:
+	CView(){}
+	~CView(){}
+};
+class CFrameWnd:public CWnd{
+public:
+	CFrameWnd(){}
+	~CFrameWnd(){}
+};
+class CDocument:public CCmdTarget
+{
+public:
+	CDocument(){}
+	~CDocument(){}
 };
